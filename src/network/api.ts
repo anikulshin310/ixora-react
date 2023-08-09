8e2c87183e315aa8327fe5118fe7d90b7ba13cfc
Andrey Nikulshin <anikulshin310@gmail.com>
2023-08-09 19:31:06 +0300

    axios+sass

---
+++ src/network/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const BASE_URL = "https://ixora-auto.ru/wipers";
+
+export const instance = axios.create({ baseURL: BASE_URL });
+
+export async function fetchData(route: string) {
+  try {
+    const response = await instance.get(route);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
---
+++ src/network/api_routes.ts
@@ -0,0 +1,6 @@
+export const API_ROUTES = {
+  AUTO: "/auto",
+  MODEL: (id: number) => `/model/${id}`,
+  MODIFICATIONS: (id: number) => `/modif/${id}`,
+  MODIFICATION_PARAMS: (id: number) => `/param/${id}`,
+};
